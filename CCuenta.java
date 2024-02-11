a5b25c2a51296707442bd557d56d9ae8498e1c7c
enricvh <159709078+enricvh@users.noreply.github.com>
2024-02-11 17:40:02 +0100

    Update CCuenta.java

--- CCuenta.java
+++ CCuenta.java
@@ -1,3 +1,6 @@
+/**
+ * Clase CCuenta que representa una cuenta bancaria.
+ */
 public class CCuenta {
 
     private String nombre;
@@ -5,9 +8,19 @@ public class CCuenta {
     private double saldo;
     private double tipoInterés;
 
+    /**
+     * Constructor por defecto de CCuenta.
+     */
     public CCuenta() {
     }
 
+    /**
+     * Constructor de CCuenta.
+     * @param nom Nombre del titular de la cuenta.
+     * @param cue Número de la cuenta.
+     * @param sal Saldo inicial de la cuenta.
+     * @param tipo Tipo de interés de la cuenta.
+     */
     public CCuenta(String nom, String cue, double sal, double tipo) {
         nombre = nom;
         cuenta = cue;
@@ -15,16 +28,30 @@ public class CCuenta {
         tipoInterés = tipo;
     }
 
+    /**
+     * Método para obtener el saldo de la cuenta.
+     * @return El saldo de la cuenta.
+     */
     public double estado() {
         return saldo;
     }
 
+    /**
+     * Método para ingresar dinero en la cuenta.
+     * @param cantidad Cantidad a ingresar.
+     * @throws Exception Si la cantidad es negativa.
+     */
     public void ingresar(double cantidad) throws Exception {
         if (cantidad < 0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
 
+    /**
+     * Método para retirar dinero de la cuenta.
+     * @param cantidad Cantidad a retirar.
+     * @throws Exception Si la cantidad es negativa o si no hay suficiente saldo.
+     */
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
             throw new Exception("No se puede retirar una cantidad negativa");
@@ -32,37 +59,4 @@ public class CCuenta {
             throw new Exception("No se hay suficiente saldo");
         saldo = saldo - cantidad;
     }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCuenta() {
-        return cuenta;
-    }
-
-    public void setCuenta(String cuenta) {
-        this.cuenta = cuenta;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-
-    public double getTipoInterés() {
-        return tipoInterés;
-    }
-
-    public void setTipoInterés(double tipoInterés) {
-        this.tipoInterés = tipoInterés;
-    }
 }
-
